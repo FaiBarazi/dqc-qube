939855665c058580b291553bcb0ad2faba6d606a
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-01 11:32:09 -0600

    Fix test case to test a proper instaitaited vector
    As opposed to 0 statevector which does not tell much.

--- tests/test_evaluation_pipeline.py
+++ tests/test_evaluation_pipeline.py
@@ -1,27 +1,30 @@
 from pathlib import Path
+from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 from pipeline.evaluation_pipeline import compute_fidelity, evolve_state, load_circuit
 
 
-def test_load_circuit_full_adder_qasm():
+def test_pipeline():
     qasm_path = (
         Path(__file__).resolve().parents[1] / "pipeline" / "full_adder_alg_4.qasm"
     )
-    circuit = load_circuit(str(qasm_path))
-
+    # Compose the full adder circuit with the input state |1010>
+    adder_circuit = load_circuit(str(qasm_path))
     assert circuit.num_qubits == 4
     assert circuit.num_clbits == 4
+    
+    circuit = QuantumCircuit(circuit.num_qubits, circuit.num_clbits)
+    circuit.x(1)
+    circuit.x(2)
+    circuit.compose(adder_circuit, inplace=True)
+   
+    
     assert len(circuit.data) == 2
     assert circuit.data[0].operation.name == "FullAdder"
 
-
-def test_evolve_state_and_compute_fidelity_zero_state():
-    qasm_path = (
-        Path(__file__).resolve().parents[1] / "pipeline" / "full_adder_alg_4.qasm"
-    )
-    circuit = load_circuit(str(qasm_path))
     output_state = evolve_state(circuit)
-    target_state = Statevector.from_label("0" * circuit.num_qubits)
+    # Target state is |1010> after applying the full adder circuit to the input state |1010>.
+    target_state = Statevector.from_label("1010")
 
     assert output_state == target_state
     assert compute_fidelity(output_state, target_state) - 1.0 <= 0.001
