bcee54836e7421dc7b1b1408c953cd2ea3e290b1
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-02 11:18:22 -0600

    Add test for a parity function oracle in the Deutsch-Josza algorithm
    Test against a scrambled target given a certain
    threshold for fidelity.

--- tests/openqasm_circuits/dj_alg_5.qasm
+++ tests/openqasm_circuits/dj_alg_5.qasm
@@ -30,7 +30,3 @@ h q[1];
 h q[2];
 h q[3];
 barrier q[0], q[1], q[2], q[3], q[4];
-c[0] = measure q[0];
-c[1] = measure q[1];
-c[2] = measure q[2];
-c[3] = measure q[3];
--- tests/test_evaluation_pipeline.py
+++ tests/test_evaluation_pipeline.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 from pipeline.evaluation_pipeline import compute_fidelity, evolve_state, load_circuit
@@ -6,7 +7,7 @@ from pipeline.evaluation_pipeline import compute_fidelity, evolve_state, load_ci
 current_dir = Path(__file__).parent
 
 
-def test_pipeline():
+def test_full_adder_pipeline():
     qasm_path = current_dir / "openqasm_circuits" / "full_adder_alg_4.qasm"
 
     adder_circuit = load_circuit(str(qasm_path))
@@ -25,3 +26,44 @@ def test_pipeline():
 
     assert output_state == target_state
     assert compute_fidelity(output_state, target_state) - 1.0 <= 0.001
+
+
+def test_deutsch_josza_pipeline():
+    """
+    The Oracle in this case is a parity function that depends on the number of
+    1 bits in a state. 
+    ex: 
+    - |0000> -> 0, |0011> -> 0, |0101> -> 0, ...
+    - |0001> -> 1, |0010> -> 1, ...
+
+    The statevector without the measuring would be: 
+    ∣ψ⟩= 1/(sqrt(2))(|01111⟩- |11111⟩)
+    There is a rounding error with floating point depending on the machine, hence we 
+    need to compare with a margin of errror. 
+    """
+    qasm_path = current_dir / "openqasm_circuits" / "dj_alg_5.qasm"
+
+    dj_circuit = load_circuit(str(qasm_path))
+    dj_circuit.decompose(reps=10).draw()
+    assert dj_circuit.num_qubits == 5
+
+    output_state = evolve_state(dj_circuit)
+    # The max amplitude is for the state |01111> and |11111> with a value of 1/sqrt(2)
+    # These are at index 15 and 31 respectively. Scramble the rest of the output_state
+    # vector to generate a target and check if the fidelity is within a margin of error. 
+    target_state = output_state.copy()
+    noise_real = np.random.uniform(-0.01, 0.01, size=target_state.data.shape)
+    noise_imag = np.random.uniform(-0.001, 0.001, size=target_state.data.shape)
+    noise = noise_real + 1j * noise_imag
+    target_state = target_state + noise
+
+    # Re normalize to make sure we have a correct vector.
+    target_state = target_state / np.linalg.norm(target_state)
+    print("Fidelity is:", compute_fidelity(output_state, target_state))
+
+    assert compute_fidelity(output_state, target_state) - 1.0 <= 0.01
+
+
+
+
+
