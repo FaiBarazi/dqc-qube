cad56de8e945b14c5cc3cca2d236935ba180998d
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-15 09:38:34 -0600

    Refactor Move actions logic to main function

--- pipeline/train_pipeline.py
+++ pipeline/train_pipeline.py
@@ -8,25 +8,20 @@ from mqt.predictor.ml import setup_device_predictor
 TARGETS = [
     "ibm_eagle_127",
     "ibm_falcon_127",
-    # "ibm_falcon_27",
-    # "ibm_heron_133",
-    # "ibm_heron_156",
-    # "ionq_aria_25",
-    # "ionq_forte_36",
-    # "iqm_crystal_20",
-    # "iqm_crystal_5",
-    # "iqm_crystal_54",
-    # "quantinuum_h2_56",
-    # "rigetti_ankaa_84"
+    "ibm_falcon_27",
+    "ibm_heron_133",
+    "ibm_heron_156",
+    "ionq_aria_25",
+    "ionq_forte_36",
+    "iqm_crystal_20",
+    "iqm_crystal_5",
+    "iqm_crystal_54",
+    "quantinuum_h2_56",
+    "rigetti_ankaa_84"
 ]
 
 
-# openQASM 3.0 includes classical control flow which is not 
-# supported by BQSKIT. By passing BQSKIT syntehsizer and compiler altogether
-# in the training. 
-for name, action in list(actions._ACTIONS.items()):
-    if action.origin == actions.CompilationOrigin.BQSKIT:
-        actions.remove_action(name)
+
 def train_optimize_devices(devices_names:list[str], figure_of_merit:str="expected_fidelity", timesteps:int=1000):
     """
     Optimize circuit compilation for specific devices using Reinforcement Learning
@@ -51,11 +46,14 @@ def train_best_device(devices_names:list[str], figure_of_merit:str="expected_fid
 
 
 def main():
-    # for name, action in list(actions._ACTIONS.items()):
-    #     if action.origin == actions.CompilationOrigin.BQSKIT:
-            # actions.remove_action(name)
-    # print("Training RL")
-    # train_optimize_devices(TARGETS)
+    # openQASM 3.0 includes classical control flow which is not 
+    # supported by BQSKIT. By passing BQSKIT syntehsizer and compiler altogether
+    # in the training. 
+    for name, action in list(actions._ACTIONS.items()):
+        if action.origin == actions.CompilationOrigin.BQSKIT:
+            actions.remove_action(name)
+    print("Training RL")
+    train_optimize_devices(TARGETS)
     print("Training ML")
     train_best_device(TARGETS)
 
