def82fc718e1bf05352214570b1f4b048f08951b
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-15 09:24:00 -0600

    Add ML training to train pipeline and refactor code

--- pipeline/train_pipeline.py
+++ pipeline/train_pipeline.py
@@ -2,6 +2,7 @@ from mqt.predictor.rl import Predictor as RL_Predictor
 from mqt.bench.targets import get_device
 import mqt.predictor.rl.actions as actions
 from mqt.predictor.reward import figure_of_merit
+from mqt.predictor.ml import setup_device_predictor
 
 
 TARGETS = [
@@ -38,5 +39,25 @@ def train_optimize_devices(devices_names:list[str], figure_of_merit:str="expecte
         except Exception as e:
             print(f"Device training failed with error: {e}")
 
+def train_best_device(devices_names:list[str], figure_of_merit:str="expected_fidelity")->bool:
+    """
+    Uses Random Forests to predict the best device given a cricuit. This needs to be trained as 
+    well on avialable devices, given a figure of merit. 
+    """
+    
+    devices = [get_device(device_name) for device_name in devices_names]
+    is_success = setup_device_predictor(devices=devices, figure_of_merit=figure_of_merit, timeout=600)
+    return is_success
 
 
+def main():
+    # for name, action in list(actions._ACTIONS.items()):
+    #     if action.origin == actions.CompilationOrigin.BQSKIT:
+            # actions.remove_action(name)
+    # print("Training RL")
+    # train_optimize_devices(TARGETS)
+    print("Training ML")
+    train_best_device(TARGETS)
+
+if __name__== "__main__":
+    main()
