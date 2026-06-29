05f9d20ae70e80d8b148757cdfbcf25a0c951f80
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-29 16:47:26 -0600

    Remove real amplitude ansatz.

--- problems/vqe_real_amp/starter.py
+++
@@ -1,8 +0,0 @@
-from qiskit import QuantumCircuit
-
-
-def solve() -> QuantumCircuit:
-    circuit = QuantumCircuit(5)
-    # TODO: Implement the VQE RealAmplitudes ansatz (reps=3, linear entanglement)
-    # with the fixed parameter values given in problem.md.
-    return circuit
