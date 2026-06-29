f4b03544944e344a42a7028d363e973f6d8651be
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-29 15:35:21 -0600

    Add starter code to h2 vqe

--- problems/h2_vqe/starter.py
+++ problems/h2_vqe/starter.py
@@ -1,8 +1,37 @@
+import numpy as np
 from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp, Statevector
 
 
-def solve() -> QuantumCircuit:
-    circuit = QuantumCircuit(2)
-    # TODO: Prepare the Bell state (|00> + |11>) / sqrt(2).
-    return circuit
+theta_1 = 0.0 # First param here. 
+theta_2 = 0.0 # Second param here. 
+
+def hamiltonian():
+    """
+    Define the Hamiltonian. Construct and return the A sparse Pauli matrix 
+    representing the H2 molecule.
+    Note: Qiskit uses little-endian qubit ordering (Qubit 1 (tensor) Qubit 0).
+    """
+    # Your code here.
+    
+    pass
+
+def ansatz(theta_1, theta_2):
+    """
+    Ansatz preparation. Construct and return the 2-qubit parameterized circuit
+    """
+    qc = QuantumCircuit(2)
+    # Your code here.
+    return qc
+
+def expectation_value(theta_1, theta_2):
+    """
+    Return the expectation value <(θ)Ψ|H|Ψ(θ)>. 
+    """
+
+def solve(theta_1, theta_2)-> float:
+    """
+    Return the expectation value given the 2 params theta_1 and theta_2
+    """
+    pass
 
