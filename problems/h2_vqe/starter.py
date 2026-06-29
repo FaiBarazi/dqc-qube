8d26dd2fc82a4fa2d418d8996d1015075694051d
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-06-29 14:07:22 -0600

    Populate VQE H2 problem folder and add README

---
+++ problems/h2_vqe/starter.py
@@ -0,0 +1,8 @@
+from qiskit import QuantumCircuit
+
+
+def solve() -> QuantumCircuit:
+    circuit = QuantumCircuit(2)
+    # TODO: Prepare the Bell state (|00> + |11>) / sqrt(2).
+    return circuit
+
