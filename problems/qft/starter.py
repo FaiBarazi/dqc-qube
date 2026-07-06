ac147a5dd7080f52e798f2f4707133d14af39a99
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2026-07-06 15:24:05 -0600

    Add starter code for Steane error correction.

--- problems/qft/starter.py
+++ problems/qft/starter.py
@@ -1,8 +1,22 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 
 
-def solve() -> QuantumCircuit:
-    circuit = QuantumCircuit(5)
-    # TODO: Prepare QFT state.
-    return circuit
+def solve(num_encoding_qubits, num_ancilla_qubits) -> QuantumCircuit:
+    # For the steane code, we need 
+    data = QuantumRegister(num_encoding_qubits, 'data')
+    ancilla = QuantumRegister(num_ancilla_qubits, 'ancilla')
+    # Used to store the measurments to get the syndrome.
+    syndrome = ClassicalRegister(num_ancilla_qubits, 'syndrome')
+    qc = QuantumCircuit(data, ancilla, syndrome)
+
+    # 1. Input Encoding here.
+
+
+    # 2.Error Detection Code here.
+ 
+
+    #3. Measure Syndrome here.
+
+    return qc
+
 
